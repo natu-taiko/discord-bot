36a452a532ae2669269e678ad33474a29dded3d0
natu-taiko <antengliu88@gmail.com>
2026-04-30 23:24:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,3 @@
-import discord
-from discord.ext import commands
-import os
-
-intents = discord.Intents.default()
-intents.message_content = True
-
-bot = commands.Bot(command_prefix="!", intents=intents)
-
-@bot.event
-async def on_ready():
-    print(f"ログイン成功: {bot.user}")
-
-@bot.event
-async def on_message(message):
-    if message.author.bot:
-        return
-
-    if "うお" in message.content:
-        await message.channel.send("冷笑まじかwwwwwwww")
-
-    await bot.process_commands(message)
-
-bot.run(os.getenv("TOKEN"))
-
 @bot.command()
 async def join(ctx):
     if not ctx.author.voice:
@@ -47,3 +22,26 @@ async def leave(ctx):
 @bot.event
 async def on_command_error(ctx, error):
     print("エラー内容:", error)
+
+import discord
+from discord.ext import commands
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"ログイン成功: {bot.user}")
+
+@bot.event
+async def on_message(message):
+    if message.author.bot:
+        return
+
+    if "うお" in message.content:
+        await message.channel.send("冷笑まじかwwwwwwww")
+bot.run(os.getenv("TOKEN"))
+await bot.process_commands(message)
